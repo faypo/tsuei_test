32c925abe4c61419072eb3e827b44885e6e9cd48
Charlotte Wong <simuyan0229@gmail.com>
2024-10-20 15:15:34 +0800

    [update]修改陳列圖間隔

--- main.js
+++ main.js
@@ -20,13 +20,13 @@ document.getElementById("up").addEventListener("click", function () {
 
 //單項商品商品圖swiper
 var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 10,
+  spaceBetween: 8,
   slidesPerView: 3,
   freeMode: true,
   watchSlidesProgress: true,
 });
 var swiper2 = new Swiper(".mySwiper2", {
-  spaceBetween: 8,
+  spaceBetween: 0,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
